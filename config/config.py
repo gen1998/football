85e22abf9ab697c1e546ce15699f7790f0c926f6
gen1998 <hamham10011@gmail.com>
2022-10-10 16:26:36 +0900

    移籍市場の選手を入団させるときに、もっとポジションの受け皿を増やす

--- config/config.py
+++ config/config.py
@@ -1,15 +1,15 @@
 ALL_POSITON_LOW = ["ST", "CAM", "RW", "RM", "CM", "CDM", "RB", "CB", "RWB"]
 ALL_POSITON_LOW_GK = ["ST", "CAM", "RW", "RM", "CM", "CDM", "RB", "CB", "RWB", "GK"]
 ALL_POSITON = ["ST", "CF", "CAM", "RW", "LW", "RM", "LM", "CM", "CDM", "RB", "LB", "CB", "RWB", "LWB"]
-POSITION_LOW_DICT = {"ST" : ["ST"],
-                     "CAM" : ["CF", "CAM"],
-                     "RW" : ["RW", "LW"],
-                     "RM" : ["RM", "LM"],
-                     "CM" : ["CM"],
-                     "CDM" : ["CDM"],
-                     "RB" : ["RB", "LB", "RWB", "LWB"],
-                     "CB" : ["CB"],
-                     "RWB" : ["RB", "LB", "RWB", "LWB"],
+POSITION_LOW_DICT = {"ST" : ["ST", "CF", "CAM"],
+                     "CAM" : ["CF", "CAM", "LW", "RW"],
+                     "RW" : ["RW", "LW", "CAM", "RM", "LW", "ST"],
+                     "RM" : ["RM", "LM", "RW", "LW"],
+                     "CM" : ["CM", "CAM", "CDM"],
+                     "CDM" : ["CDM", "CB", "CM", "RB", "LB"],
+                     "RB" : ["RB", "LB", "RWB", "LWB", "RM", "LM"],
+                     "CB" : ["CB", "RB", "LB", "CDM"],
+                     "RWB" : ["RB", "LB", "RWB", "LWB", "RM", "LM"],
                      "GK" : ["GK"]}
 
 N = 1000000
